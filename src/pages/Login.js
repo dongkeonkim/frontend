e7c064cc793b32df6229f14f4c0fac8df77a75e8
dongkeonkim <dongkeonkim0810@gmail.com>
2023-12-15 00:11:01 +0900

    design: 로그인 페이지 디자인 수정

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -14,12 +14,22 @@ export default function Login() {
         />
         <div class="my-1 w-1/2 flex justify-start">
           <input type="checkbox" class="mr-2" />
-          <label>자동 로그인</label>
+          <label class="text-sm">자동 로그인</label>
         </div>
         <div class="my-1 w-1/2 h-10 bg-pink-100">
           <button class="text-white bg-blue-700 font-medium text-lg w-full h-full rounded-lg">
             로그인
           </button>
+        </div>
+        <div class="flex justify-between w-1/2 my-1">
+          <div class="text-sm">
+            <label>아이디</label>
+            <label>/</label>
+            <label>비밀번호 찾기</label>
+          </div>
+          <div class="text-sm">
+            <label>회원가입</label>
+          </div>
         </div>
       </div>
     </div>
